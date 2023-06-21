4c80ae4a102068f8425734bcec8a5c8e79683173
schafmeister <johanna.schafmeister@rwth-aachen.de>
2023-06-21 15:45:55 +0200

    changes to cosmo_emulator

--- looti/cosmo_emulator.py
+++ looti/cosmo_emulator.py
@@ -4,55 +4,46 @@ import numpy as np
 
 class CosmoEmulator:
 
-    def __init__(self, cosmo_quantities, cosmo_inputs, data_path):
-        
-        self.cosmo_quantities = cosmo_quantities
-        self.cosmo_inputs = cosmo_inputs
-        self.data_path = data_path
+    def __init__(self):
 
-        self.num_parameters = cosmo_inputs.shape[-1]
-
+        self.data = {}
         self.intobjs = {}
 
 
-    def read_data(self, n_train, n_test):
+    def read_data(self, cosmo_quantity, data_path, n_params, n_train, n_test, **kwargs):
 
-        self.data = {}
+        data_folder = data_path
+        datafile_ext = cosmo_quantity
+        datafile_ref = cosmo_quantity + '_ref'
 
-        for quant in self.cosmo_quantities:
-
-            data_folder = self.data_path
-            datafile_ext = quant
-            datafile_ref = quant + '_ref'
-
-            emulation_data = dhl.DataHandle(datafile_ext,
-                                            data_folder,
-                                            datafile_ref,
-                                            num_parameters=self.num_parameters,
-                                            data_type=quant,
-                                            features_name='grid',
-                                            features_to_Log=True,
-                                            normalize_by_reference=True,
-                                            normalize_by_mean_std=True) 
-            emulation_data.read_csv_pandas()
-            emulation_data.calculate_ratio_by_redshifts(emulation_data.z_vals)
-            emulation_data.calculate_data_split(n_train=n_train,
-                                                n_test=n_test,
-                                                verbosity=0,
-                                                manual_split=True)
-            emulation_data.data_split()
-            
-            self.data[quant] = emulation_data
+        emulation_data = dhl.DataHandle(datafile_ext,
+                                        data_folder,
+                                        datafile_ref,
+                                        num_parameters=n_params,
+                                        data_type=cosmo_quantity,
+                                        features_name=kwargs.get('features_name', 'grid'),
+                                        features_to_Log=kwargs.get('features_to_Log', True),
+                                        normalize_by_reference=True,
+                                        normalize_by_mean_std=True) 
+        emulation_data.read_csv_pandas()
+        emulation_data.calculate_ratio_by_redshifts(emulation_data.z_vals)
+        emulation_data.calculate_data_split(n_train=n_train,
+                                            n_test=n_test,
+                                            verbosity=0,
+                                            manual_split=True)
+        emulation_data.data_split()
+        
+        self.data[cosmo_quantity] = emulation_data
 
 
-    def create_intobj(self, cosmo_quantity, ncomp):
+    def create_intobj(self, cosmo_quantity, n_params, **kwargs):
 
         emulation_data = self.data[cosmo_quantity]
 
         PCAop = dcl.LearningOperator(method='PCA', 
-                                     ncomp=ncomp, 
-                                     gp_n_rsts=40, 
-                                     gp_length=np.ones(self.num_parameters), 
+                                     ncomp=kwargs.get('ncomp', 8), 
+                                     gp_n_rsts=kwargs.get('gp_n_rsts', 40), 
+                                     gp_length=kwargs.get('gp_length', np.ones(n_params)), 
                                      verbosity=0)
         intobj = dcl.LearnData(PCAop)
         intobj.interpolate(train_data=emulation_data.matrix_datalearn_dict['train'],
@@ -61,7 +52,14 @@ class CosmoEmulator:
         self.intobjs[cosmo_quantity] = intobj    
 
 
-    def get_prediction(self, cosmo_quantity, input_params, redshift=None):
+    def get_prediction(self, cosmo_quantity, input_dict, redshift=None):
+
+        emulation_data = self.data[cosmo_quantity]
+
+        input_params_list = []
+        for param in emulation_data.paramnames_dict.values():
+            input_params_list.append(input_dict[param])
+        input_params = np.array(input_params_list)
 
         if redshift == None:
             params_requested = input_params.reshape(1, -1)
@@ -74,6 +72,8 @@ class CosmoEmulator:
         predicted = intobj.predict(params_requested)
         prediction_reconstructed = dcl.reconstruct_spectra(ratios_predicted=predicted, 
                                                            emulation_data=emulation_data)
+        
+        fgrid = emulation_data.fgrid
 
-        return prediction_reconstructed[list(prediction_reconstructed.keys())[0]]
+        return fgrid, prediction_reconstructed[list(prediction_reconstructed.keys())[0]]
     
