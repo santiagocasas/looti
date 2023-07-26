c5668322fdad8abcb4ad2b986f87e944db1891cf
Santiago Casas <santiagocasas@gmail.com>
2023-07-26 15:42:48 +0200

    continuation of implementation

--- looti/cosmicfish_theory.py
+++ looti/cosmicfish_theory.py
@@ -15,31 +15,43 @@ class LootiFish(CosmoEmulator):
         super().__init__()
         self.training_args = training_args
         # Read data from path/csv files for each cosmological quantity in extra_args
-        self.must_provide = ['Plin']
+        self.must_provide = ['Plin', 'Pnonlin', 'sigma8', 'f_GrowthRate', 'background_H']
         for quantity in self.training_args['quantities']:
             if quantity in self.must_provide:
             # If there exists path to trained intobj, read it
                 if 'emulator_path' in self.training_args['quantities'][quantity].keys():
-                    self.read_emulator(quantity, self.training_args['quantities'][quantity]['emulator_path'])
+                    self.read_emulator(cosmo_quantity=quantity, 
+                                       data_path=self.training_args['quantities'][quantity]['data_path'],
+                                       emulator_path=self.training_args['quantities'][quantity]['emulator_path']
+                                       )
         
-        self.z_grid = training_args['quantities']['z_grid']
-
+        #self.z_grid = training_args['quantities']['z_grid']
+        self.z_grid = 
         return None
 
     def set_args(self, kwargs):
         self.training_args = kwargs
                 
-    def get_kgrid(self, params_values_dict, nonlinear=False, units="Mpc"):
+    def get_kgrid(self, params_values_dict, units="Mpc"):
         # Get Pk from intobj
-        kgrid, Pk = self.get_prediction('Plin')
+        kgrid, Pk = self.get_prediction(cosmo_quantity='Plin',
+                                        input_dict=params_values_dict)
         return kgrid 
+    
+    def get_zgrid(self, params_values_dict, units="Mpc"):
+        # Get Pk from intobj
+        zgrid, Hub = self.get_prediction(cosmo_quantity='background_H',
+                                        input_dict=params_values_dict)
+        return zgrid 
     
     def compute_Pk(self, params_values_dict, nonlinear=False, units="Mpc"):
         # Get Pk from intobj
         if nonlinear == False:
-            kgrid, Pk = self.get_prediction('Plin')
+            kgrid, Pk = self.get_prediction(cosmo_quantity='Plin',
+                                            input_dict=params_values_dict)
         elif nonlinear ==  True:
-            kgrid, Pk = self.get_prediction('Pnonlin')
+            kgrid, Pk = self.get_prediction(cosmo_quantity='Pnonlin',
+                                            input_dict=params_values_dict)
         
         Pkz = np.array((len(self.z_grid), len(kgrid)))
         for ii,zz in enumerate(self.z_grid):
