cb53b6228abc94ac403f597fe7f3730f6d74c3e2
Johanna Schafmeister <johanna.schafmeister@rwth-aachen.de>
2023-10-17 11:37:26 +0200

    small changes

--- notebooks/pandas_delete_col.py
+++ notebooks/pandas_delete_col.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import os
 
-load_path = '../training_data/justClsLensed_10sigma_sig8/'
-files = ['lensed_TT.csv.zip', 'lensed_TE.csv.zip', 'lensed_EE.csv.zip']
-ref_files = ['lensed_TT_ref.csv.zip', 'lensed_TE_ref.csv.zip', 'lensed_EE_ref.csv.zip']
+load_path = '../training_data/w0wa_sig8/'
+files = ['_Plin.csv.zip', '_Plincb.csv.zip', '_Pnonlin.csv.zip', '_Pnonlincb.csv.zip', 
+         '_background_H.csv.zip', '_sigma8.csv.zip', '_D_Growth.csv.zip', '_f_GrowthRate.csv.zip']
+         # '_TT.csv.zip', '_TE.csv.zip', '_EE.csv.zip']
+ref_files = ['_Plin_ref.csv.zip', '_Plincb_ref.csv.zip', '_Pnonlin_ref.csv.zip', '_Pnonlincb_ref.csv.zip', 
+         '_background_H_ref.csv.zip', '_sigma8_ref.csv.zip', '_D_Growth_ref.csv.zip', '_f_GrowthRate_ref.csv.zip']
+         # '_TT_ref.csv.zip', '_TE_ref.csv.zip', '_EE_ref.csv.zip']
 
-save_path = '../training_data/justClsLensed_10sigma_nosig8/'
+save_path = '../training_data/w0wa_nosig8/'
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
@@ -13,7 +17,7 @@ if not os.path.exists(save_path):
 for file in files:
     print(file)
     df = pd.read_csv(load_path + file)
-    df_drop = df.drop(columns=['parameter_7', 'parameter_7_value'])
+    df_drop = df.drop(columns=['parameter_8', 'parameter_8_value'])
     df_drop.to_csv(save_path + file, index=False)
     print('saved')
 
